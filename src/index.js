c4ef1ebdb3282dd9a843ebfc88a84232255ed0c2
”0xAureoleNode“ <aureolenode@gmail.com>
2022-10-27 15:40:54 +0800

    avatar component

--- src/index.js
+++ src/index.js
@@ -3,7 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 function Tweet() {
-  return <div className="tweet">Tweet</div>;
+  return (
+    <div className="tweet">
+      <Avatar />
+      Tweet
+    </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <img
+      src="https://www.gravatar.com/avatar/e839af16ed505a9ecba821461987356a"
+      alt="avatar"
+      className="avatar"
+    />
+  );
 }
 
 ReactDOM.render(<Tweet />, document.querySelector('#root'));

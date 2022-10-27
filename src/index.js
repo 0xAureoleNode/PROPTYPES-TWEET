e6be358b8bc989f9f7671aea17356c4107db2068
”0xAureoleNode“ <aureolenode@gmail.com>
2022-10-27 16:06:16 +0800

    basic component

--- src/index.js
+++ src/index.js
@@ -6,7 +6,17 @@ function Tweet() {
   return (
     <div className="tweet">
       <Avatar />
-      Tweet
+      <div className="content">
+        <Message />
+        <Author />
+        <Time />
+        <div className="buttons">
+          <ReplyButton />
+          <RetweetButton />
+          <LikeButton />
+          <MoreOptionsButton />
+        </div>
+      </div>
     </div>
   );
 }
@@ -20,5 +30,30 @@ function Avatar() {
     />
   );
 }
+
+function Message() {
+  return <div className="message">This is less than 140 characters</div>;
+}
+
+function Author() {
+  return (
+    <span className="author">
+      <span className="name">Your name</span>
+      <span className="handle">@yourhandle</span>
+    </span>
+  );
+}
+
+const Time = () => <span className="time">3h ago</span>;
+
+const ReplyButton = () => <i className="fa fa-reply reply-button" />;
+
+const RetweetButton = () => <i className="fa fa-retweet retweet-button" />;
+
+const LikeButton = () => <i className="fa fa-heart like-button" />;
+
+const MoreOptionsButton = () => (
+  <i className="fa fa-ellipsis-h more-options-button" />
+);
 
 ReactDOM.render(<Tweet />, document.querySelector('#root'));

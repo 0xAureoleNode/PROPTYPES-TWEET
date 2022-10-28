4db49ff11c54de989a0723ad79cd692cd04e69ce
”0xAureoleNode“ <aureolenode@gmail.com>
2022-10-28 13:38:23 +0800

    prop types

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
 import moment from 'moment';
+import PropTypes from 'prop-types';
+import { createRoot } from 'react-dom/client';
 
 function Tweet({ tweet }) {
   return (
@@ -22,14 +23,26 @@ function Tweet({ tweet }) {
   );
 }
 
+Tweet.propTypes = {
+  tweet: PropTypes.object.isRequired,
+};
+
 function Avatar({ hash }) {
   const url = `https://www.gravatar.com/avatar/${hash}`;
   return <img src={url} alt="avatar" className="avatar" />;
 }
 
+Avatar.propTypes = {
+  hash: PropTypes.string.isRequired,
+};
+
 function Message({ text }) {
   return <div className="message">{text}</div>;
 }
+
+Message.propTypes = {
+  text: PropTypes.string,
+};
 
 function Author({ author }) {
   const { name, handle } = author;
@@ -41,9 +54,20 @@ function Author({ author }) {
   );
 }
 
+Author.propTypes = {
+  author: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    handle: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
 const Time = ({ time }) => {
   const timeString = moment(time).fromNow();
   return <span className="time">{timeString}</span>;
+};
+
+Time.propTypes = {
+  time: PropTypes.string,
 };
 
 const ReplyButton = () => <i className="fa fa-reply reply-button" />;
@@ -62,12 +86,20 @@ const RetweetButton = ({ count }) => (
   </span>
 );
 
+RetweetButton.propTypes = {
+  count: PropTypes.number,
+};
+
 const LikeButton = ({ count }) => (
   <span className="like-button">
     <i className="fa fa-heart" />
     {count > 0 && <span className="like-count">{count}</span>}
   </span>
 );
+
+LikeButton.propTypes = {
+  count: PropTypes.number,
+};
 
 const MoreOptionsButton = () => (
   <i className="fa fa-ellipsis-h more-options-button" />
@@ -85,4 +117,6 @@ const testTweet = {
   timestamp: '2022-10-27 21:24:37',
 };
 
-ReactDOM.render(<Tweet tweet={testTweet} />, document.querySelector('#root'));
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<Tweet tweet={testTweet} />);
